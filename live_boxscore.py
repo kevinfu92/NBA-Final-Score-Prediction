32f3cb8086ab1759883856d3f04ce69ffd19c617
kevinfu92 <kfu47@gatech.edu>
2024-05-05 20:49:26 -0600

    Added ability to pull data at halftime, predict, and send email
    -Added live_boxscore.py to obtain boxscore at halftime when available
    -Updated find_all_nba_gameids to be able to specify the type of games
    -Added the ability to fit the model and predict the final score
    -Added the ability to send predictions via email at halftime

---
+++ live_boxscore.py
@@ -0,0 +1,193 @@
+import os
+
+from nba_api.stats.endpoints import boxscoretraditionalv3
+from nba_api.stats.endpoints import leaguegamefinder
+from nba_api.live.nba.endpoints import scoreboard
+from nba_api.live.nba.endpoints import boxscore
+import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
+import numpy as np
+from nba_api.stats.static import teams
+import json
+from datetime import date
+from main import *
+import re
+from scipy import stats
+import math
+import smtplib
+from email.mime.text import MIMEText
+
+csv_for_cleaning_name = str(date.today()) + '_nba_for_cleaning.csv'
+csv_for_analysis_name = str(date.today()) + '_nba_for_analysis.csv'
+result_csv_name = str(date.today()) + '_nba_prediction.csv'
+def live_boxscore(game_ids, csv):
+    final = pd.DataFrame(
+        columns=['GAME_ID', 'home_team', 'away_team', 'hs_minutes', 'hs_FGM', 'hs_FGA', 'hs_3PM', 'hs_3PA', 'hs_FTM',
+                 'hs_FTA', 'hs_OREB', 'hs_DREB', 'hs_AST', 'hs_STL', 'hs_BLK', 'hs_TO', 'hs_FOUL',
+                 'ht_minutes', 'ht_FGM', 'ht_FGA', 'ht_3PM', 'ht_3PA', 'ht_FTM', 'ht_FTA',
+                 'ht_OREB', 'ht_DREB', 'ht_AST', 'ht_STL', 'ht_BLK', 'ht_TO', 'ht_FOUL',
+                 'as_minutes', 'as_FGM', 'as_FGA', 'as_3PM', 'as_3PA', 'as_FTM', 'as_FTA',
+                 'as_OREB', 'as_DREB', 'as_AST', 'as_STL', 'as_BLK', 'as_TO', 'as_FOUL',
+                 'at_minutes', 'at_FGM', 'at_FGA', 'at_3PM', 'at_3PA', 'at_FTM', 'at_FTA',
+                 'at_OREB', 'at_DREB', 'at_AST', 'at_STL', 'at_BLK', 'at_TO', 'at_FOUL'])
+
+    for id in game_ids:
+        data = json.loads(boxscore.BoxScore(id).get_json())['game']
+        game_data = create_boxscore_df(data, id, 'live')
+        final.loc[len(final)] = game_data
+
+    final.to_csv(csv, index=False)
+
+    return final
+
+
+def transform_data(input_csv, output_csv):
+    '''
+    See NBA Data Cleaning.ipynb for data cleaning steps
+    :param csv: csv file pulled using live_boxscore
+    :return: None
+    '''
+    boxscore = pd.read_csv(input_csv)
+    # Calculate home team bench stats
+    boxscore['hb_FGM'] = boxscore['ht_FGM'] - boxscore['hs_FGM']
+    boxscore['hb_FGA'] = boxscore['ht_FGA'] - boxscore['hs_FGA']
+    boxscore['hb_3PM'] = boxscore['ht_3PM'] - boxscore['hs_3PM']
+    boxscore['hb_3PA'] = boxscore['ht_3PA'] - boxscore['hs_3PA']
+    boxscore['hb_FTM'] = boxscore['ht_FTM'] - boxscore['hs_FTM']
+    boxscore['hb_FTA'] = boxscore['ht_FTA'] - boxscore['hs_FTA']
+    boxscore['hb_OREB'] = boxscore['ht_OREB'] - boxscore['hs_OREB']
+    boxscore['hb_DREB'] = boxscore['ht_DREB'] - boxscore['hs_DREB']
+    boxscore['hb_AST'] = boxscore['ht_AST'] - boxscore['hs_AST']
+    boxscore['hb_STL'] = boxscore['ht_STL'] - boxscore['hs_STL']
+    boxscore['hb_BLK'] = boxscore['ht_BLK'] - boxscore['hs_BLK']
+    boxscore['hb_TO'] = boxscore['ht_TO'] - boxscore['hs_TO']
+    boxscore['hb_FOUL'] = boxscore['ht_FOUL'] - boxscore['hs_FOUL']
+    # Calculate away team bench stats
+    boxscore['ab_FGM'] = boxscore['at_FGM'] - boxscore['as_FGM']
+    boxscore['ab_FGA'] = boxscore['at_FGA'] - boxscore['as_FGA']
+    boxscore['ab_3PM'] = boxscore['at_3PM'] - boxscore['as_3PM']
+    boxscore['ab_3PA'] = boxscore['at_3PA'] - boxscore['as_3PA']
+    boxscore['ab_FTM'] = boxscore['at_FTM'] - boxscore['as_FTM']
+    boxscore['ab_FTA'] = boxscore['at_FTA'] - boxscore['as_FTA']
+    boxscore['ab_OREB'] = boxscore['at_OREB'] - boxscore['as_OREB']
+    boxscore['ab_DREB'] = boxscore['at_DREB'] - boxscore['as_DREB']
+    boxscore['ab_AST'] = boxscore['at_AST'] - boxscore['as_AST']
+    boxscore['ab_STL'] = boxscore['at_STL'] - boxscore['as_STL']
+    boxscore['ab_BLK'] = boxscore['at_BLK'] - boxscore['as_BLK']
+    boxscore['ab_TO'] = boxscore['at_TO'] - boxscore['as_TO']
+    boxscore['ab_FOUL'] = boxscore['at_FOUL'] - boxscore['as_FOUL']
+    # Drop team stats for both home and away
+    boxscore.drop(list(boxscore.filter(regex='t_')), axis=1, inplace=True)
+    # Create home_away column
+    home_away = ['home']*boxscore.shape[0] + ['away']*boxscore.shape[0]
+    # Create duplicate row since it only pulled 1 row per game
+    boxscore = pd.concat([boxscore, boxscore], axis=0)
+    # Add home_away column
+    boxscore['home_away'] = home_away
+
+    # Split combined_df to home_df and away_df
+    home_df = boxscore[boxscore['home_away'] == 'home'].drop(
+        list(boxscore.filter(regex='as_|ab_|TEAM_ABBREVIATION')), axis=1)
+    away_df = boxscore[boxscore['home_away'] == 'away'].drop(
+        list(boxscore.filter(regex='hs_|hb_|TEAM_ABBREVIATION')), axis=1)
+
+    # Rename columns
+    home_df = home_df.rename({'home_team': 'team', 'away_team': 'against'}, axis=1).rename(
+        columns=lambda x: re.sub(r'h(s|b)_', r'\1_', x)).reset_index(drop=True)
+    away_df = away_df.rename({'away_team': 'team', 'home_team': 'against'}, axis=1).rename(
+        columns=lambda x: re.sub(r'a(s|b)_', r'\1_', x)).reset_index(drop=True)
+
+    # Combine home_df and away_df
+    final_df = pd.concat([home_df, away_df], ignore_index=True, axis=0)
+    # Total score at halftime
+    final_df['PTS_ht'] = final_df.apply(
+        lambda x: (x['s_FGM'] + x['b_FGM'] - (x['s_3PM'] + x['b_3PM'])) * 2 + (x['s_3PM'] + x['b_3PM']) * 3 + (
+                    x['s_FTM'] + x['b_FTM']) * 1, axis=1)
+    # Create column for opponent point at halftime
+    final_df['GAME_ID_agst'] = final_df.apply(lambda x: str(x['GAME_ID']) + x['against'], axis=1)
+    final_df['GAME_ID_team'] = final_df.apply(lambda x: str(x['GAME_ID']) + x['team'], axis=1)
+    # Create dataframe for opponent point at halftime only, use this to merge
+    opp_pts = final_df[['GAME_ID_agst', 'PTS_ht']]
+    # Merge final_df and opp_pts
+    final_df = final_df.merge(opp_pts, left_on='GAME_ID_team', right_on='GAME_ID_agst')
+    # Drop uncessary columns
+    final_df.drop(columns=['GAME_ID_team', 'GAME_ID_agst_x', 'GAME_ID_agst_y'], inplace=True)
+    # Rename columns
+    final_df.rename(columns={'PTS_ht_y': 'PTS_ht_opp', 'PTS_ht_x': 'PTS_ht'}, inplace=True)
+    # Rename and replace value in home column
+    final_df.rename(columns={'home_away': 'home'}, inplace=True)
+    final_df['home'] = final_df['home'].apply(lambda x: 1 if x=='home' else 0)
+
+    final_df.to_csv(output_csv, index=False)
+def fit_model(input_csv=csv_for_analysis_name, output_csv=result_csv_name):
+    '''
+    See NBA-Analysis.html for model. All values were copies from the model
+    :param csv: csv file name for analysis, string
+    :return: No return. Generates results and output to a csv file
+    '''
+    # Model = PTS_final ~ s_minutes + s_3PM + s_3PA + s_DREB + s_BLK + s_TO + log(b_FTM + 1) + log(b_OREB + 1) +
+    #   #   b_DREB +  b_AST + log(b_STL + 1) + b_FOUL + PTS_ht + PTS_ht_opp + home
+    data = pd.read_csv(input_csv)
+    # TODO fix this prediction interval. this is a very rough estimate
+    t_value = stats.t.ppf(0.875, 1950) #75% confidence interval
+    MSE = 8.844
+    pred_error = t_value * (MSE**0.5)
+    # Convert b_FTM, b_OREB, b_STL to log(value)+1 for calculation
+    for colname in ['b_FTM', 'b_OREB', 'b_STL']:
+        data[colname] = data[colname].apply(lambda x: math.log(x)+1)
+
+
+    prediction = 43.5287 - 0.0907*data['s_minutes'] - 0.38002*data['s_3PM'] + 0.2126*data['s_3PA'] +\
+                  0.23784*data['s_DREB'] + 0.19932*data['s_BLK'] + 0.20365*data['s_TO'] -\
+                  0.55545*data['b_FTM'] + 0.76769*data['b_OREB'] + 0.37672*data['b_DREB'] - \
+                  0.1769*data['b_AST'] - 0.56588*data['b_STL'] + 0.1734*data['b_FOUL'] + \
+                  1.04884*data['PTS_ht'] + 0.18728*data['PTS_ht_opp'] + 1.02333*data['home']
+
+    data['pred_lb'] = round(prediction - pred_error, 1)
+    data['pred'] = round(prediction, 1)
+    data['pred_ub'] = round(prediction + pred_error, 1)
+    data = data[['team', 'against', 'PTS_ht', 'pred_lb', 'pred', 'pred_ub']]
+    data.loc['total'] = data.sum()
+    data.to_csv(output_csv, index=False)
+
+def send_email():
+    sender_email = os.environ['sender_email']
+    sender_password = os.environ['sender_password']
+    recipient_email = os.environ['recipient_email']
+    subject = f"NBA Prediction"
+    body = """\
+    <html>
+      <head></head>
+      <body>
+        {0}
+      </body>
+    </html>
+    """.format(pd.read_csv(result_csv_name).to_html())
+    html_message = MIMEText(body, 'html')
+    html_message['Subject'] = subject
+    html_message['From'] = sender_email
+    html_message['To'] = recipient_email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, recipient_email, html_message.as_string())
+
+
+# Uses 2023 Playoff Games to test this function
+def get_gameids_today():
+    gameids_today = []
+    live_scoreboard = scoreboard.ScoreBoard()
+    board = live_scoreboard.get_dict()
+    for game in board['scoreboard']['games']:
+        # TODO Change and check status for halftime
+        if (game["period"] == 2 and game["gameClock"] == "PT00M00.00S") or (game['gameStatusText'].lower() == "halftime"):
+            gameids_today.append(game['gameId'])
+    live_boxscore(gameids_today, csv=csv_for_cleaning_name)
+    transform_data(input_csv=csv_for_cleaning_name, output_csv=csv_for_analysis_name)
+    fit_model(csv_for_analysis_name, result_csv_name)
+    send_email()
+
+
+
+get_gameids_today()
+
+
--- main.py
+++ main.py
@@ -14,10 +14,17 @@ def find_all_nba_teamids():
         team_ids.append(t['id'])
     return team_ids
 
-def find_all_nba_gameids(season="2023"):
+def find_all_nba_gameids(season="2023", season_type='Regular Season'):
+
+    '''
+    :param season: the NBA season in year (ex. "2023"), string
+    :param season_type: type of season, ^(Regular Season)|(Pre Season)|(Playoffs)$
+    :return: all NBA game ids that meet specified conditions
+    '''
+
 
     team_ids = find_all_nba_teamids()
-    gamefinder = leaguegamefinder.LeagueGameFinder(season_type_nullable='Regular Season')
+    gamefinder = leaguegamefinder.LeagueGameFinder(season_type_nullable=season_type)
     games = gamefinder.get_data_frames()[0]
 
     # Convert SEASON_ID to string and split to SEASON_TYPE and SEASON_YEAR
@@ -28,67 +35,129 @@ def find_all_nba_gameids(season="2023"):
 
     # Filter only regular season NBA games in 2023-2024 season
     games = games.query('TEAM_ID in @team_ids & SEASON_YEAR == @season & GAME_ID < 1000000000')
+    games.to_csv('test.csv')
     # Add leading 00 to GAME_ID
     games['GAME_ID'] = games['GAME_ID'].apply(lambda x: '00' + str(x))
     game_ids = games['GAME_ID'].unique()
 
     return game_ids
 
-def boxscore_at_HT(game_ids='game_id.csv'):
-    final = pd.DataFrame(columns=['GAME_ID', 'home_team','away_team','hs_minutes','hs_FGM','hs_FGA', 'hs_3PM', 'hs_3PA', 'hs_FTM',
-                                  'hs_FTA', 'hs_OREB', 'hs_DREB', 'hs_AST', 'hs_STL', 'hs_BLK', 'hs_TO', 'hs_FOUL',
-                                  'ht_minutes', 'ht_FGM', 'ht_FGA', 'ht_3PM', 'ht_3PA', 'ht_FTM', 'ht_FTA', 
-                                  'ht_OREB', 'ht_DREB', 'ht_AST', 'ht_STL', 'ht_BLK', 'ht_TO', 'ht_FOUL',
-                                  'as_minutes', 'as_FGM', 'as_FGA', 'as_3PM', 'as_3PA', 'as_FTM', 'as_FTA',
-                                  'as_OREB', 'as_DREB', 'as_AST', 'as_STL', 'as_BLK', 'as_TO', 'as_FOUL',
-                                  'at_minutes', 'at_FGM', 'at_FGA', 'at_3PM', 'at_3PA', 'at_FTM', 'at_FTA',
-                                  'at_OREB', 'at_DREB', 'at_AST', 'at_STL', 'at_BLK', 'at_TO', 'at_FOUL'])
+def create_boxscore_df(data, g, data_source='static'):
+    '''
 
-
-    game_id_df = pd.read_csv(game_ids)
-    ids = game_id_df['GAME_ID']
-    for g in ids:
-        games = boxscoretraditionalv3.BoxScoreTraditionalV3(game_id='00'+str(g), end_range=14400, end_period=10,
-                                                            range_type=2, start_period=1, start_range=0)
-
-        data = json.loads(games.get_json())
-
-        # Pull starter and bench stats (note that boxscoretraditionalv3 is not pulling bench stats correctly.
-        # Use team stats minus starter stats to obtain bench stats
+    :param data: boxscore pulled from BoxScoreTraditionalV3 and converted to json, json
+    :param g: game_id, int
+    :param data_source: source of data, static (from boxscoretraditionalv3) or live (from boxscore), string
+    :return: boxscore with starter, team, home, and away data, json
+    '''
+    # Use team stats minus starter stats to obtain bench stats
+    if data_source == 'static':
         home_team = data['boxScoreTraditional']['homeTeam']['teamTricode']
         away_team = data['boxScoreTraditional']['awayTeam']['teamTricode']
         home_starter_stat = data['boxScoreTraditional']['homeTeam']['starters']
         home_team_stat = data['boxScoreTraditional']['homeTeam']['statistics']
         away_starter_stat = data['boxScoreTraditional']['awayTeam']['starters']
         away_team_stat = data['boxScoreTraditional']['awayTeam']['statistics']
+    elif data_source == 'live':
+        home_team = data['homeTeam']['teamTricode']
+        away_team = data['awayTeam']['teamTricode']
 
-        game_data = [g, home_team, away_team, home_starter_stat['minutes'], home_starter_stat['fieldGoalsMade'],
-                         home_starter_stat['fieldGoalsAttempted'], home_starter_stat['threePointersMade'],
-                         home_starter_stat['threePointersAttempted'],home_starter_stat['freeThrowsMade'],
-                         home_starter_stat['freeThrowsAttempted'], home_starter_stat['reboundsOffensive'],
-                         home_starter_stat['reboundsDefensive'], home_starter_stat['assists'], home_starter_stat['steals'],
-                         home_starter_stat['blocks'], home_starter_stat['turnovers'], home_starter_stat['foulsPersonal'], 
-                         home_team_stat['minutes'], home_team_stat['fieldGoalsMade'],
-                         home_team_stat['fieldGoalsAttempted'], home_team_stat['threePointersMade'],
-                         home_team_stat['threePointersAttempted'],home_team_stat['freeThrowsMade'],
-                         home_team_stat['freeThrowsAttempted'], home_team_stat['reboundsOffensive'],
-                         home_team_stat['reboundsDefensive'], home_team_stat['assists'], home_team_stat['steals'],
-                         home_team_stat['blocks'], home_team_stat['turnovers'], home_team_stat['foulsPersonal'],
-                         away_starter_stat['minutes'], away_starter_stat['fieldGoalsMade'],
-                         away_starter_stat['fieldGoalsAttempted'], away_starter_stat['threePointersMade'],
-                         away_starter_stat['threePointersAttempted'],away_starter_stat['freeThrowsMade'],
-                         away_starter_stat['freeThrowsAttempted'], away_starter_stat['reboundsOffensive'],
-                         away_starter_stat['reboundsDefensive'], away_starter_stat['assists'], away_starter_stat['steals'],
-                         away_starter_stat['blocks'], away_starter_stat['turnovers'], away_starter_stat['foulsPersonal'],
-                         away_team_stat['minutes'], away_team_stat['fieldGoalsMade'],
-                         away_team_stat['fieldGoalsAttempted'], away_team_stat['threePointersMade'],
-                         away_team_stat['threePointersAttempted'],away_team_stat['freeThrowsMade'],
-                         away_team_stat['freeThrowsAttempted'], away_team_stat['reboundsOffensive'],
-                         away_team_stat['reboundsDefensive'], away_team_stat['assists'], away_team_stat['steals'],
-                         away_team_stat['blocks'], away_team_stat['turnovers'], away_team_stat['foulsPersonal']]
+        # home team
+        num_player = len(data['homeTeam']['players'])
+        home_starter_stats = []
+        home_team_stats = []
+        for i in range(num_player):
+            home_team_stats.append(data['homeTeam']['players'][i]['statistics'])
+            if data['homeTeam']['players'][i]['starter'] == "1":
+                home_starter_stats.append(data['homeTeam']['players'][i]['statistics'])
+
+        home_starter_stats = pd.DataFrame(home_starter_stats)
+        home_team_stats = pd.DataFrame(home_team_stats)
+
+        ## Convert minutes from 'PT32M15.00S' to 32.15
+        home_starter_stats['minutes'] = home_starter_stats['minutes'].apply(lambda x: float(x.replace('PT', '')[:-4].replace('M', '.')))
+        home_team_stats['minutes'] = home_team_stats['minutes'].apply(lambda x: float(x.replace('PT', '')[:-4].replace('M', '.')))
+        ## Calculate sums
+        home_starter_stat = home_starter_stats.sum()
+        home_team_stat = home_team_stats.sum()
+
+        # away team
+        num_player = len(data['awayTeam']['players'])
+        away_starter_stats = []
+        away_team_stats = []
+        for i in range(num_player):
+            away_team_stats.append(data['awayTeam']['players'][i]['statistics'])
+            if data['awayTeam']['players'][i]['starter'] == "1":
+                away_starter_stats.append(data['awayTeam']['players'][i]['statistics'])
+
+        away_starter_stats = pd.DataFrame(away_starter_stats)
+        away_team_stats = pd.DataFrame(away_team_stats)
+
+        ## Convert minutes from 'PT32M15.00S' to 32.15
+        away_starter_stats['minutes'] = away_starter_stats['minutes'].apply(
+            lambda x: float(x.replace('PT', '')[:-4].replace('M', '.')))
+        away_team_stats['minutes'] = away_team_stats['minutes'].apply(
+            lambda x: float(x.replace('PT', '')[:-4].replace('M', '.')))
+        ## Calculate sums
+        away_starter_stat = away_starter_stats.sum()
+        away_team_stat = away_team_stats.sum()
+
+
+    game_data = [g, home_team, away_team, home_starter_stat['minutes'], home_starter_stat['fieldGoalsMade'],
+                 home_starter_stat['fieldGoalsAttempted'], home_starter_stat['threePointersMade'],
+                 home_starter_stat['threePointersAttempted'], home_starter_stat['freeThrowsMade'],
+                 home_starter_stat['freeThrowsAttempted'], home_starter_stat['reboundsOffensive'],
+                 home_starter_stat['reboundsDefensive'], home_starter_stat['assists'], home_starter_stat['steals'],
+                 home_starter_stat['blocks'], home_starter_stat['turnovers'], home_starter_stat['foulsPersonal'],
+                 home_team_stat['minutes'], home_team_stat['fieldGoalsMade'],
+                 home_team_stat['fieldGoalsAttempted'], home_team_stat['threePointersMade'],
+                 home_team_stat['threePointersAttempted'], home_team_stat['freeThrowsMade'],
+                 home_team_stat['freeThrowsAttempted'], home_team_stat['reboundsOffensive'],
+                 home_team_stat['reboundsDefensive'], home_team_stat['assists'], home_team_stat['steals'],
+                 home_team_stat['blocks'], home_team_stat['turnovers'], home_team_stat['foulsPersonal'],
+                 away_starter_stat['minutes'], away_starter_stat['fieldGoalsMade'],
+                 away_starter_stat['fieldGoalsAttempted'], away_starter_stat['threePointersMade'],
+                 away_starter_stat['threePointersAttempted'], away_starter_stat['freeThrowsMade'],
+                 away_starter_stat['freeThrowsAttempted'], away_starter_stat['reboundsOffensive'],
+                 away_starter_stat['reboundsDefensive'], away_starter_stat['assists'], away_starter_stat['steals'],
+                 away_starter_stat['blocks'], away_starter_stat['turnovers'], away_starter_stat['foulsPersonal'],
+                 away_team_stat['minutes'], away_team_stat['fieldGoalsMade'],
+                 away_team_stat['fieldGoalsAttempted'], away_team_stat['threePointersMade'],
+                 away_team_stat['threePointersAttempted'], away_team_stat['freeThrowsMade'],
+                 away_team_stat['freeThrowsAttempted'], away_team_stat['reboundsOffensive'],
+                 away_team_stat['reboundsDefensive'], away_team_stat['assists'], away_team_stat['steals'],
+                 away_team_stat['blocks'], away_team_stat['turnovers'], away_team_stat['foulsPersonal']]
+    return game_data
+
+def boxscore_at_HT(game_ids='game_id.csv', output_csv='NBA_2023_Halftime_Boxscore.csv'):
+    final = pd.DataFrame(columns=['GAME_ID', 'home_team','away_team','hs_minutes','hs_FGM','hs_FGA', 'hs_3PM', 'hs_3PA', 'hs_FTM',
+                                  'hs_FTA', 'hs_OREB', 'hs_DREB', 'hs_AST', 'hs_STL', 'hs_BLK', 'hs_TO', 'hs_FOUL',
+                                  'ht_minutes', 'ht_FGM', 'ht_FGA', 'ht_3PM', 'ht_3PA', 'ht_FTM', 'ht_FTA',
+                                  'ht_OREB', 'ht_DREB', 'ht_AST', 'ht_STL', 'ht_BLK', 'ht_TO', 'ht_FOUL',
+                                  'as_minutes', 'as_FGM', 'as_FGA', 'as_3PM', 'as_3PA', 'as_FTM', 'as_FTA',
+                                  'as_OREB', 'as_DREB', 'as_AST', 'as_STL', 'as_BLK', 'as_TO', 'as_FOUL',
+                                  'at_minutes', 'at_FGM', 'at_FGA', 'at_3PM', 'at_3PA', 'at_FTM', 'at_FTA',
+                                  'at_OREB', 'at_DREB', 'at_AST', 'at_STL', 'at_BLK', 'at_TO', 'at_FOUL'])
+
+    # Check if game_ids is from a csv or a list
+    if isinstance(game_ids, str) and game_ids[-4:] == '.csv':
+        game_id_df = pd.read_csv(game_ids)
+        ids = game_id_df['GAME_ID']
+    else:
+        ids = game_ids
+
+    for g in ids:
+        games = boxscoretraditionalv3.BoxScoreTraditionalV3(game_id='00'+str(g), end_range=14400, end_period=10,
+                                                            range_type=2, start_period=1, start_range=0)
+        data = json.loads(games.get_json())
+        # Pull starter and bench stats (note that boxscoretraditionalv3 is not pulling bench stats correctly.
+        # Use team stats minus starter stats to obtain bench stats
+        game_data = create_boxscore_df(data, g)
         final.loc[len(final)] = game_data
 
-    final.to_csv('NBA_2023_Halftime_Boxscore.csv')
+    final.to_csv(output_csv, index_label=False)
 
     return final
 
+
+boxscore_at_HT(game_ids=[42300137], output_csv='NBA_2023_Halftime_Boxscore1.csv')
